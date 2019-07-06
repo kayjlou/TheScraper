5930b412e22a31a3dedb4b3debe2f7451bc56846
Katie Lewis <klewi314@gmail.com>
2019-07-06 11:06:55 -0700

    added mongod uri

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const { join } = require('path')
 const app = express()
 
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/scraperdb'
+
 
 app.use(express.static(join(__dirname, 'public')))
 app.use(express.urlencoded({ extended: true }))
@@ -9,8 +11,8 @@ app.use(express.json())
 
 require('./routes')(app)
 
-require('mongoose').connect(process.env.MONGODB_URI || 'mongodb://localhost/scraperdb', { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
-  .then(_ => app.listen(3000))
+require('mongoose').connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: true })
+  .then(_ => app.listen(process.env.PORT || 3000))
   .catch(e => console.log(e))
 
 
